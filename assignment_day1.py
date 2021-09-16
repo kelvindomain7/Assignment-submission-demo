a9cf1df5894e6742c7dc44df9dec2d44c6d596a3
kelvindomain7 <53256646+kelvindomain7@users.noreply.github.com>
2021-09-16 08:36:58 +0100

    Add files via upload

---
+++ assignment_day1.py
@@ -0,0 +1,18 @@
+# Day 1 Question Number 1
+x = 2.1
+y = 2.5
+
+print (x is y)
+print (x == y)
+print (2.5 is y)
+
+a = "abc"
+#b = "xyz"
+
+print (a is a)
+
+
+# Day 1 Question Number 2
+print (1>3>4)
+
+ 
