5369e6d719721473e03da730aa7f1558315dafd5
kelvindomain7 <53256646+kelvindomain7@users.noreply.github.com>
2021-09-16 13:32:42 +0100

    Add files via upload

---
+++ assignment_day2.py
@@ -0,0 +1,15 @@
+
+	
+
+
+primenum = input("Enter number:")
+
+	
+while primenum != "3":
+
+	print("No, this is not a prime number")
+	primenum = input("Enter number again:")
+	
+ 
+
+print ("Yes, this is prime number")
